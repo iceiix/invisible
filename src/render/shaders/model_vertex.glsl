ba611e4892a19bee701aae48e1cc90410942c97b
ice_iix <ice_ix@protonmail.ch>
2018-11-27 12:15:22 -0800

    Empty model shaders

--- src/render/shaders/model_vertex.glsl
+++ src/render/shaders/model_vertex.glsl
@@ -1,22 +1,2 @@
-in vec3 aPosition;
-in vec4 aTextureInfo;
-in ivec3 aTextureOffset;
-in vec4 aColor;
-in int id;
-
-uniform mat4 perspectiveMatrix;
-uniform mat4 cameraMatrix;
-uniform mat4 modelMatrix[10];
-uniform float lightLevel;
-uniform float skyOffset;
-uniform vec2 lighting;
-
-out vec4 vColor;
-out vec4 vTextureInfo;
-out vec2 vTextureOffset;
-out float vAtlas;
-out float vID;
-out vec3 vLighting;
-
 void main() {
 }
