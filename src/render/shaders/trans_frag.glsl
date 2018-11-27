a1034d652ec87b51569a2e5f7a1ccafab2541def
ice_iix <ice_ix@protonmail.ch>
2018-11-27 12:08:53 -0800

    Remove aa

--- src/render/shaders/trans_frag.glsl
+++ src/render/shaders/trans_frag.glsl
@@ -6,11 +6,10 @@ out vec4 fragColor;
 void main() {
     ivec2 C = ivec2(gl_FragCoord.xy);
     vec4 accum = texelFetch(taccum, C, 0);
-    float aa = 1.0;
     vec4 col = texelFetch(tcolor, C, 0);
 
     float r = accum.a;
-    accum.a = aa;
+    accum.a = 1.0;
     if (r >= 1.0) {
         fragColor = vec4(col.rgb, 0.0);
     } else {
