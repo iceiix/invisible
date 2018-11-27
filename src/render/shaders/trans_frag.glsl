38a4cf460d9645149f2058c45a7e335c68c43f20
ice_iix <ice_ix@protonmail.ch>
2018-11-27 12:14:08 -0800

    Simplify trans_frag to a single line

--- src/render/shaders/trans_frag.glsl
+++ src/render/shaders/trans_frag.glsl
@@ -3,8 +3,5 @@ uniform sampler2DMS tcolor;
 out vec4 fragColor;
 
 void main() {
-    ivec2 C = ivec2(gl_FragCoord.xy);
-    vec4 col = texelFetch(tcolor, C, 0);
-
-    fragColor = vec4(col.rgb, 0.0);
+    fragColor = texelFetch(tcolor, ivec2(gl_FragCoord.xy), 0);
 }
