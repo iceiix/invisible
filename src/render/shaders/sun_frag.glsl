54ef8bb9666c0af0ea780ef683ef9f0a514669af
ice_iix <ice_ix@protonmail.ch>
2018-11-27 14:15:10 -0800

    Remove unused sun_frag uniforms and attributes

--- src/render/shaders/sun_frag.glsl
+++ src/render/shaders/sun_frag.glsl
@@ -1,11 +1,3 @@
-uniform sampler2DArray textures;
-uniform vec4 colorMul[10];
-
-in vec4 vTextureInfo;
-in vec2 vTextureOffset;
-in float vAtlas;
-in float vID;
-
 out vec4 fragColor;
 
 void main() {
