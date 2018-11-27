9f93d45556155d189085dbceca504310877c834f
ice_iix <ice_ix@protonmail.ch>
2018-11-27 10:35:56 -0800

    Remove mpx and mpy in generate_moon

--- src/sun.rs
+++ src/sun.rs
@@ -31,15 +31,13 @@ impl SunModel {
 
     pub fn generate_moon(renderer: &mut render::Renderer) -> model::ModelKey {
         let tex = render::Renderer::get_texture(renderer.get_textures_ref(), "environment/moon_phases");
-        let mpx = (0 % 4) as f64 * (1.0 / 4.0);
-        let mpy = (0 / 4) as f64 * (1.0 / 2.0);
         renderer.model.create_model(
             model::SUN,
             vec![vec![
-                model::Vertex{x: 0.0, y: -SIZE, z: -SIZE, texture_x: mpx, texture_y: mpy + (1.0 / 2.0), texture: tex.clone(), r: 255, g: 255, b: 255, a: 0, id: 0},
-                model::Vertex{x: 0.0, y: SIZE, z: -SIZE, texture_x: mpx, texture_y: mpy, texture: tex.clone(), r: 255, g: 255, b: 255, a: 0, id: 0},
-                model::Vertex{x: 0.0, y: -SIZE, z: SIZE, texture_x: mpx + (1.0 / 4.0), texture_y: mpy + (1.0 / 2.0), texture: tex.clone(), r: 255, g: 255, b: 255, a: 0, id: 0},
-                model::Vertex{x: 0.0, y: SIZE, z: SIZE, texture_x: mpx + (1.0 / 4.0), texture_y: mpy, texture: tex.clone(), r: 255, g: 255, b: 255, a: 0, id: 0}
+                model::Vertex{x: 0.0, y: -SIZE, z: -SIZE, texture_x: 0.0, texture_y: 0.0 + (1.0 / 2.0), texture: tex.clone(), r: 255, g: 255, b: 255, a: 0, id: 0},
+                model::Vertex{x: 0.0, y: SIZE, z: -SIZE, texture_x: 0.0, texture_y: 0.0, texture: tex.clone(), r: 255, g: 255, b: 255, a: 0, id: 0},
+                model::Vertex{x: 0.0, y: -SIZE, z: SIZE, texture_x: 0.0 + (1.0 / 4.0), texture_y: 0.0 + (1.0 / 2.0), texture: tex.clone(), r: 255, g: 255, b: 255, a: 0, id: 0},
+                model::Vertex{x: 0.0, y: SIZE, z: SIZE, texture_x: 0.0 + (1.0 / 4.0), texture_y: 0.0, texture: tex.clone(), r: 255, g: 255, b: 255, a: 0, id: 0}
             ]]
         )
     }
