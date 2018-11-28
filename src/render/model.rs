d6d7341ba61fbcfb47ae9d9db2b841aec68ecd16
ice_iix <ice_ix@protonmail.ch>
2018-11-27 16:27:32 -0800

    Log model matrix length

--- src/render/model.rs
+++ src/render/model.rs
@@ -155,7 +155,7 @@ impl Manager {
 
             for model in collection.models.values() {
                 model.array.bind();
-                println!("model.matrix = {:?}", &model.matrix);
+                println!("model.matrix(len={}) = {:?}", model.matrix.len(), &model.matrix);
                 collection.shader.model_matrix.map(|v| v.set_matrix4_multi(&model.matrix));
 println!("about to draw model {:?} {:?}", model.count, self.index_type);
                 gl::draw_elements(gl::TRIANGLES, model.count, self.index_type, 0);
